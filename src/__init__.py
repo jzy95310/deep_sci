235d5780f21a70d278da6215c09fd996669df421
Ziyang Jiang <ziyang.jiang@duke.edu>
2023-12-06 22:32:02 -0500

    Update ICK submodule

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from . import data_generator
 from . import models
 from . import networks
+from . import trainers
 
-__all__ = ['data_generator', 'models', 'networks']
+__all__ = ['data_generator', 'models', 'networks', 'trainers']
