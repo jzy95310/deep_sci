16a0c9edde5298a2cc42bd30a4915a6dbbdf1313
jzy95310 <45862046+jzy95310@users.noreply.github.com>
2024-08-06 08:49:25 -0700

    Re-add real-world experiments for causal effect estimation.

---
+++ experiments/geospatial_real_causal_effect_estimation/run_models.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p scavenger-gpu --account=carlsonlab --gres=gpu:1 --mem=64G
+#SBATCH --job-name=sci_geospatial_real
+#SBATCH --output=sci_geospatial_%a.out
+#SBATCH --error=sci_geospatial_%a.err
+#SBATCH -a 101-106
+#SBATCH -c 2
+#SBATCH --nice
+
+export WANDB__SERVICE_WAIT=300
+
+srun --cpu_bind=cores singularity exec --nv --bind /work/zj63 /datacommons/carlsonlab/Containers/multimodal_gp.simg python run_models.py
